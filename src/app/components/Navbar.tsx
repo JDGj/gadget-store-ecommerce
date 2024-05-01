6aa2ba5206e6fd12d287bd08c4f13a64dffe31a4
JDGj <joaodgaiteiro@gmail.com>
2024-05-01 19:13:33 +0100

    Updt navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <div className="flex justify-between align-middle bg-slate-600">
+      <div className="flex justify-between align-middle bg-slate-600 p-10">
         NAVBAR
       </div>
     </>
